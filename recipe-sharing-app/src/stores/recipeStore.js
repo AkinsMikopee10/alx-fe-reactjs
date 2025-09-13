bde7e7e54a7e619c0f7d3a431215e61dc55ee78b
Michael Akinyemi <akinsmikopee2507@gmail.com>
2025-09-13 21:02:12 +0100

    Initialize the Recipe Sharing Application with Zustand State Management

---
+++ recipe-sharing-app/src/stores/recipeStore.js
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+// Zustand store for recipes
+const useRecipeStore = create((set) => ({
+  recipes: [],
+
+  // Action to add a new recipe
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  // Action to initialize the recipe list
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
