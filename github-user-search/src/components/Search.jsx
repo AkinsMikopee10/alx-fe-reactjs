48706a3562256d40db360514c40697ad8f54403f
Michael Akinyemi <akinsmikopee2507@gmail.com>
2025-09-21 18:13:34 +0100

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -21,7 +21,7 @@ function Search() {
       setUser(data);
     } catch (err) {
       console.error(err);
-      setError("Looks like we can’t find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
